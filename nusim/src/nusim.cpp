87a31c0362b829dd9d71fc4376147bf85ec73474
Maximiliano Palay <maximilianopalay@gmail.com>
2024-01-21 17:03:25 -0600

    added comments and doxygen docs

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,3 +1,24 @@
+/// \file
+/// \brief Custom simulator core implementation.
+///
+/// PARAMETERS:
+///     rate (double): frequency [hz] of updates of the simulation
+///     x0 (double): initial x location [m] of the turtlebot
+///     y0 (double): initial y location [m] of the turtlebot
+///     theta0 (double): initial angle [rad] of the turtlebot
+///     arena_x_length (double): x length [m] of the arena
+///     arena_y_length (double): y length [m] of the arena
+///     obstacles/x (std::vector<double>): x locations [m] of the obstacles
+///     obstacles/y (std::vector<double>): y locations [m] of the obstacles
+///     obstacles/r (std::vector<double>): radius [m] of the obstacles
+/// PUBLISHES:
+///     ~/walls (visualization_msgs::msg::MarkerArray): marker array containing the markers of the environment walls
+///     ~/obstacles (visualization_msgs::msg::MarkerArray): marker array containing the obstacles
+///     ~/timestep (std_msgs::msg::UInt64): publishes the simulation time (timestep at rate frequency)
+/// SERVERS:
+///     ~/reset (std_srvs::srv::Empty): resets turtlebot location and simulation time
+///     ~/teleport (nusim_interfaces::srv::Teleport): teleports the turtlebot to a new location
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -30,10 +51,11 @@ public:
     declare_parameter("theta0", 0.0);
     declare_parameter("arena_x_length", 0.0);
     declare_parameter("arena_y_length", 0.0);
-    declare_parameter("obstacles/x", std::vector<double>{0.1});
-    declare_parameter("obstacles/y", std::vector<double>{0.1});
+    declare_parameter("obstacles/x", std::vector<double>());
+    declare_parameter("obstacles/y", std::vector<double>());
     declare_parameter("obstacles/r", std::vector<double>());
 
+    // check if the obstacles arrays are of corresponding sizes
     check_obstacles();
 
     // create the reset service and bind it
@@ -60,17 +82,19 @@ public:
     // calculate timer step
     std::chrono::milliseconds timerStep =
       (std::chrono::milliseconds)(int)(1000.0 / get_parameter("rate").as_double());
+
+    // create time publisher
     timestep_publisher_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
 
     // create main loop timer
     timer_ = create_wall_timer(
       timerStep, std::bind(&NuSim::timer_callback, this));
 
-    //
+    // create QoS for markers publishers
     auto markers_qos_ = rclcpp::SystemDefaultsQoS{};
     markers_qos_.transient_local();
 
-    //
+    // create walls publisher
     walls_publisher_ = create_publisher<visualization_msgs::msg::MarkerArray>(
       "~/walls",
       markers_qos_);
@@ -78,42 +102,55 @@ public:
     // publish arena walls
     publish_arena_walls();
 
-    //
+    // create obstacles publisher
     obstacles_publisher_ = create_publisher<visualization_msgs::msg::MarkerArray>(
       "~/obstacles",
       markers_qos_);
+
+    // publish obstacles
     publish_obstacles();
 
   }
 
 private:
+  // main loop
   void timer_callback()
   {
+    // publish timestep update, and update timestep
     auto message = std_msgs::msg::UInt64();
     message.data = timestep_;
-    RCLCPP_INFO_STREAM(get_logger(), timestep_);
     timestep_publisher_->publish(message);
+    timestep_++;
 
+    // output timestep to screen
+    RCLCPP_INFO_STREAM(get_logger(), timestep_);
+
+    // broadcast transform
     turtle_pose_.header.stamp = get_clock()->now();
     tf_broadcaster_->sendTransform(turtle_pose_);
-
-    timestep_++;
   }
+
+  // callback for the reset service
   void reset_callback(
     const std::shared_ptr<std_srvs::srv::Empty::Request>,
     std::shared_ptr<std_srvs::srv::Empty::Response>)
   {
+    // set the turtle back to initial location
     set_initial_location();
+    // reset timestep
     timestep_ = 0;
   }
 
+  // callback for the teleport service
   void teleport_callback(
     const std::shared_ptr<nusim_interfaces::srv::Teleport::Request> request,
     std::shared_ptr<nusim_interfaces::srv::Teleport::Response>)
   {
+    // directly set the x, y location
     turtle_pose_.transform.translation.x = request->x;
     turtle_pose_.transform.translation.y = request->y;
 
+    // translate input yaw into quaternion
     tf2::Quaternion q;
     q.setRPY(0, 0, request->theta);
     turtle_pose_.transform.rotation.x = q.x();
@@ -122,18 +159,23 @@ private:
     turtle_pose_.transform.rotation.w = q.w();
   }
 
+  // helper function to set the initial location of the turtle
   void set_initial_location()
   {
+    // grab the initial location parameters
     turtle_pose_.transform.translation.x = get_parameter("x0").as_double();
     turtle_pose_.transform.translation.y = get_parameter("y0").as_double();
+    // translate yaw into quaternion
     tf2::Quaternion q;
     q.setRPY(0, 0, get_parameter("theta0").as_double());
+    // set quaternion
     turtle_pose_.transform.rotation.x = q.x();
     turtle_pose_.transform.rotation.y = q.y();
     turtle_pose_.transform.rotation.z = q.z();
     turtle_pose_.transform.rotation.w = q.w();
   }
 
+  // helper function to create a wall marker
   visualization_msgs::msg::Marker create_wall(
     double scaleX, double scaleY, double scaleZ,
     double posX, double posY, double posZ, double id)
@@ -158,6 +200,7 @@ private:
     return marker;
   }
 
+  // helper function to create an obstacle marker
   visualization_msgs::msg::Marker create_obstacle(
     double scaleX, double scaleY, double scaleZ,
     double posX, double posY, double posZ, double id)
@@ -182,6 +225,7 @@ private:
     return marker;
   }
 
+  // helper function to check the lengths of the arguments obstacles/* are the same
   void check_obstacles()
   {
     auto obstacles_x = get_parameter("obstacles/x").as_double_array();
@@ -194,6 +238,7 @@ private:
     }
   }
 
+  // helper function to publish obstacles
   void publish_obstacles()
   {
     auto cylinder_height = 0.25;
@@ -215,6 +260,7 @@ private:
     obstacles_publisher_->publish(marker_array);
   }
 
+  // helper function to publish the arena walls
   void publish_arena_walls()
   {
     auto wall_thickness = 0.1;
@@ -250,6 +296,7 @@ private:
         wall_thickness, y_size + 2.0 * wall_thickness, wall_height,
         -(x_size + wall_thickness) / 2.0, 0, wall_z_pos, 3));
 
+    // publish
     walls_publisher_->publish(marker_array);
   }
 
@@ -262,6 +309,7 @@ private:
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   geometry_msgs::msg::TransformStamped turtle_pose_;
   u_int64_t timestep_;
+
 };
 
 int main(int argc, char * argv[])
