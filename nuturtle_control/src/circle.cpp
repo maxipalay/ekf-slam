d55dba9ff93d905e9a1aa08dbc6d5b066b5e26bd
Maximiliano Palay <maximilianopalay@gmail.com>
2024-02-07 01:33:51 -0600

    added circle node, created control interface, update Cmakelists

---
+++ nuturtle_control/src/circle.cpp
@@ -0,0 +1,117 @@
+/// \file
+/// \brief Control the turtle in a circular motion.
+///
+/// PARAMETERS:
+///     frequency (double): frequency for message publishing (hz)
+/// PUBLISHES:
+///     cmd_vel (geometry_msgs::msg:Twist): velocity commands
+/// SERVICES:
+///     control (nuturtle_control_interfaces::srv::Control): control the trajectory
+///     reverse (std_msgs::srv::Empty): reverses direction of robot
+///     stop (std_msgs::srv::Empty): stops the trajectory execution
+
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_srvs/srv/empty.hpp"
+#include "nuturtle_control_interfaces/srv/control.hpp"
+
+using namespace std::chrono_literals;
+
+
+class Circle : public rclcpp::Node
+{
+public:
+  Circle()
+  : Node("circle")
+  {
+    // parameters declaration
+    declare_parameter("frequency", 100.0);
+
+    // calculate timer step
+    std::chrono::milliseconds timerStep =
+      (std::chrono::milliseconds)(int)(1000.0 / get_parameter("frequency").as_double());
+
+    // create main loop timer
+    timer_ = create_wall_timer(
+      timerStep, std::bind(&Circle::timer_callback, this));
+
+    // create services
+    control_service_ =
+      create_service<nuturtle_control_interfaces::srv::Control>(
+      "control",
+      std::bind(&Circle::control_callback, this, std::placeholders::_1, std::placeholders::_2));
+    
+    reverse_service_ =
+      create_service<std_srvs::srv::Empty>(
+      "reverse",
+      std::bind(&Circle::reverse_callback, this, std::placeholders::_1, std::placeholders::_2));
+    
+    stop_service_ =
+      create_service<std_srvs::srv::Empty>(
+      "stop",
+      std::bind(&Circle::stop_callback, this, std::placeholders::_1, std::placeholders::_2));
+
+    // create publishers and subscribers
+    vel_publisher_ = create_publisher<geometry_msgs::msg::Twist>(
+      "cmd_vel", 10);
+    
+  }
+
+private:
+
+  void timer_callback(){
+    if (!stop_ack){
+        if (stop_flg){
+            cmd_vel.linear.x = 0;
+            cmd_vel.angular.z = 0;
+            stop_ack = true;
+            stop_flg = false;
+        }
+        vel_publisher_->publish(cmd_vel);
+    }
+  }
+
+  void control_callback(const std::shared_ptr<nuturtle_control_interfaces::srv::Control::Request> request,
+    std::shared_ptr<nuturtle_control_interfaces::srv::Control::Response>){
+        stop_ack = false;
+        cmd_vel.linear.x = request->velocity;
+        cmd_vel.angular.z = request->velocity / request->radius;
+  }
+
+  void reverse_callback(const std::shared_ptr<std_srvs::srv::Empty::Request>,
+    std::shared_ptr<std_srvs::srv::Empty::Response>){
+    cmd_vel.linear.x *= -1.0;
+    cmd_vel.angular.z *= -1.0;
+  }
+
+  void stop_callback(const std::shared_ptr<std_srvs::srv::Empty::Request>,
+    std::shared_ptr<std_srvs::srv::Empty::Response>){
+    stop_flg = true;
+  }
+ 
+  rclcpp::TimerBase::SharedPtr timer_;
+  u_int64_t timestep_;
+  geometry_msgs::msg::Twist cmd_vel;
+  bool stop_flg;
+  bool stop_ack;
+  
+  rclcpp::Service<nuturtle_control_interfaces::srv::Control>::SharedPtr control_service_;
+  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reverse_service_;
+  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr stop_service_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_publisher_;
+
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Circle>());
+  rclcpp::shutdown();
+  return 0;
+}
