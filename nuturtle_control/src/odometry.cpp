c1217abb4214bef5115fa85ee8e1e2df5937caa2
Maximiliano Palay <maximilianopalay@gmail.com>
2024-02-07 01:30:44 -0600

    added loop to publish odom data at constant intervals

--- nuturtle_control/src/odometry.cpp
+++ nuturtle_control/src/odometry.cpp
@@ -8,6 +8,7 @@
 ///     odom_id (string): name of the odometry frame
 ///     wheel_left (string): name of the left wheel link
 ///     wheel_right (string): name of the right wheel link
+///     rate (double): rate at which odometry is published (default 100)
 /// PUBLISHES:
 ///     odom (nav_msgs::msg::Odometry): turtlebot odometry
 ///     tf (tf2 transform broadcast): transform between odom frame and body frame
@@ -49,6 +50,7 @@ public:
     declare_parameter("wheel_left", rclcpp::ParameterType::PARAMETER_STRING);
     declare_parameter("wheel_right", rclcpp::ParameterType::PARAMETER_STRING);
     declare_parameter("odom_id", "odom");
+    declare_parameter("rate", 100.0);
 
     wheelRadius = get_parameter("wheel_radius").as_double();
     wheelTrack = get_parameter("track_width").as_double();
@@ -56,6 +58,10 @@ public:
     nameWheelLeft = get_parameter("wheel_left").as_string();
     nameWheelRight = get_parameter("wheel_right").as_string();
     odomId = get_parameter("odom_id").as_string();
+
+    // calculate timer step
+    std::chrono::milliseconds timerStep =
+      (std::chrono::milliseconds)(int)(1000.0 / get_parameter("rate").as_double());
 
     // initialize odom data
     odomMsg.header.frame_id = odomId;
@@ -81,10 +87,18 @@ public:
       create_service<nuturtle_control_interfaces::srv::InitialPose>(
       "initial_pose",
       std::bind(&Odometry::pose_callback, this, std::placeholders::_1, std::placeholders::_2));
+
+    // create main loop timer
+    timer_ = create_wall_timer(
+      timerStep, std::bind(&Odometry::timer_callback, this));
     
   }
 
 private:
+  void timer_callback(){
+    pubOdom_->publish(odomMsg);
+    tf_broadcaster_->sendTransform(odomTransform);
+  }
  
   void pose_callback(
     const std::shared_ptr<nuturtle_control_interfaces::srv::InitialPose::Request> request,
@@ -111,33 +125,36 @@ private:
 
   void joints_cb(const sensor_msgs::msg::JointState & msg){
     auto timeNow = get_clock()->now();
-    auto dt = (timeNow - lastJointData).seconds();
-    auto transform = ddrive.FKin(msg.position[0], msg.position[1]);
-    odomMsg.header.stamp = timeNow;
-    odomMsg.pose.pose.position.x = transform.translation().x;
-    odomMsg.pose.pose.position.y = transform.translation().y;
-    odomMsg.pose.pose.orientation.x = 0.0; // will always be zero in planar rotations
-    odomMsg.pose.pose.orientation.y = 0.0; // will always be zero in planar rotations
-    odomMsg.pose.pose.orientation.z = std::sin(transform.rotation()/2.0);
-    odomMsg.pose.pose.orientation.w = std::cos(transform.rotation()/2.0);
-    odomMsg.twist.twist.linear.x = (transform.translation().x-lastTransform.translation().x)/(dt);
-    odomMsg.twist.twist.linear.y = (transform.translation().y-lastTransform.translation().y)/(dt);
-    odomMsg.twist.twist.angular.z = (transform.rotation()-lastTransform.rotation())/(dt);
-    pubOdom_->publish(odomMsg);
+    auto transform = ddrive.FKin(msg.position.at(0), msg.position.at(1));
+    if (!first_joints_cb){
+        auto dt = (timeNow - lastJointData).seconds();
+        odomMsg.header.stamp = timeNow;
+        odomMsg.pose.pose.position.x = transform.translation().x;
+        odomMsg.pose.pose.position.y = transform.translation().y;
+        odomMsg.pose.pose.orientation.x = 0.0; // will always be zero in planar rotations
+        odomMsg.pose.pose.orientation.y = 0.0; // will always be zero in planar rotations
+        odomMsg.pose.pose.orientation.z = std::sin(transform.rotation()/2.0);
+        odomMsg.pose.pose.orientation.w = std::cos(transform.rotation()/2.0);
+        odomMsg.twist.twist.linear.x = (transform.translation().x-lastTransform.translation().x)/(dt);
+        odomMsg.twist.twist.linear.y = (transform.translation().y-lastTransform.translation().y)/(dt);
+        odomMsg.twist.twist.angular.z = (transform.rotation()-lastTransform.rotation())/(dt);
+        // pubOdom_->publish(odomMsg);
 
-    odomTransform.header.stamp = timeNow;
-    odomTransform.transform.translation.x = odomMsg.pose.pose.position.x;
-    odomTransform.transform.translation.y = odomMsg.pose.pose.position.y;
-    odomTransform.transform.rotation.x = odomMsg.pose.pose.orientation.x;
-    odomTransform.transform.rotation.y = odomMsg.pose.pose.orientation.y;
-    odomTransform.transform.rotation.z = odomMsg.pose.pose.orientation.z;
-    odomTransform.transform.rotation.w = odomMsg.pose.pose.orientation.w;
-    tf_broadcaster_->sendTransform(odomTransform);
-
+        odomTransform.header.stamp = timeNow;
+        odomTransform.transform.translation.x = odomMsg.pose.pose.position.x;
+        odomTransform.transform.translation.y = odomMsg.pose.pose.position.y;
+        odomTransform.transform.rotation.x = odomMsg.pose.pose.orientation.x;
+        odomTransform.transform.rotation.y = odomMsg.pose.pose.orientation.y;
+        odomTransform.transform.rotation.z = odomMsg.pose.pose.orientation.z;
+        odomTransform.transform.rotation.w = odomMsg.pose.pose.orientation.w;
+        // tf_broadcaster_->sendTransform(odomTransform);
+    } else {
+        first_joints_cb = false;
+    }
     lastJointData = timeNow;
     lastTransform = transform;
   }
-
+  bool first_joints_cb = true;
   double wheelRadius;
   double wheelTrack;
   std::string bodyId;
@@ -153,6 +170,7 @@ private:
   rclcpp::Time lastJointData;
   turtlelib::Transform2D lastTransform;
   rclcpp::Service<nuturtle_control_interfaces::srv::InitialPose>::SharedPtr pose_service_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
 };
 
